6efc51701df7501536fd4af52dea5f10c3b5db34
Nikolozub <NikolaSamsonov@mail.ru>
2020-10-23 11:12:16 +0700

    dobavil otchot

--- WorkWithOpenMP/WorkWithOpenMP.cpp
+++ WorkWithOpenMP/WorkWithOpenMP.cpp
@@ -1,4 +1,6 @@
 ﻿#include <iostream>
+#include <ctime>
+#include <math.h>
 #include "omp.h"
 
 using namespace std;
@@ -7,9 +9,15 @@ double fun(double x);
 
 int main() {
 
+    setlocale(0, "");
+
+    int k;
     double a = 0, b = 0;
     int n = 0;
     double sum = 0;
+
+    cout << "Введите число потоков: ";
+    cin >> k;
 
     cout << "a: ";
     cin >> a;
@@ -17,17 +25,23 @@ int main() {
     cin >> b;
     cout << "n: ";
     cin >> n;
-    
-    double h = fabs((b - a) / n);
 
-    #pragma omp parallel for num_threads(2) reduction (+: sum)
+    double h = (b - a) / n;
+
+    unsigned int start_time = clock(); // начальное время
+
+    #pragma omp parallel for num_threads(k) reduction (+: sum)
     for (int i = 1; i <= n; ++i)
         sum += fun(a + i * h) * h;
 
-    printf("result: %f", sum);
+    unsigned int end_time = clock(); // конечное время
+
+    cout << "result: " << sum << endl;
+    cout << "time: " << (end_time - start_time) << endl;
+
     return 0;
 }
 
 double fun(double x) {
-    return 10.0;
+    return (cos(2 * x * x) * sin(x) + log(x * x * x)) - (cos(2 * x * x) / sin(x) - log(x * x * x));
 }
